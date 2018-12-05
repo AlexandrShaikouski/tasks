ad1e2b65b07d624b99a5b95f51e658e765c782c5
Александр <sash_shay@mail.ru>
2018-12-05 22:10:04 +0300

    Add task

---
+++ src/task/FirstTask.java
@@ -0,0 +1,100 @@
+package task;
+
+import java.util.Scanner;
+
+public class FirstTask {
+    private static Scanner scanner = new Scanner(System.in);
+    private static FirstTask ft = new FirstTask();
+    private static double cathetusFirst, cathetusSecond;
+    private static final int[] A = new int[]{4,5,2,3,22,11,55,33,677,44,33,22};
+    private static final int K = 5;
+    private static String str = " Mad worLd ";
+    private static double[] d1 = {3.2, 5.21, 6.7, 2.32, 22.4, 88.21, 33.2};
+    private static double[] d2 = {3.2, 5.21, 6.7, 22.4, 88.21};
+
+    public static void main(String[] args) {
+        System.out.println("Hello World\n");                                                           // 1 clause
+
+        System.out.println("ФИО:     Шайковский Александр Александрович\n" +                           // 2 clause
+                           "Адрес:   РБ г. Минск ул. Казинца д. 102 кв. 218\n" +                       // 2 clause
+                           "Телефон: +375-25-618-24-21\n");                                            // 2 clause
+
+        System.out.println("Введите размеры Катетов: ");                                               // 3 clause
+        System.out.print("Первый катет равен: ");                                                      // 3 clause
+        cathetusFirst = scanner.nextDouble();                                                          // 3 clause
+        System.out.print("Второй катет равен: ");                                                      // 3 clause
+        cathetusSecond = scanner.nextDouble();                                                         // 3 clause
+        System.out.println();                                                                          // 3 clause
+
+        System.out.println("Гипотенуза равна " + ft.getHypotenuse(cathetusFirst, cathetusSecond));     // 3 clause
+        System.out.println("Площадь равна    " + ft.getSquare(cathetusFirst, cathetusSecond));         // 3 clause
+        System.out.println();
+
+        System.out.println("Сумма натуральных чисел кратных " + K + " равна " + ft.getSummMultiples());// 4 clause
+        System.out.println();
+
+        ft.showFiveMethodOfString(str);                                                                // 5 clause
+        System.out.println();
+
+        ft.monotoneSequence(d1);                                                                       // 6 clause
+        ft.monotoneSequence(d2);                                                                       // 6 clause
+
+    }
+
+    public double getHypotenuse(double cathetusFirst, double cathetusSecond){
+        double hypotenuse = Math.sqrt(Math.pow(cathetusFirst,2) + Math.pow(cathetusSecond,2));
+        return hypotenuse;
+    }                      // 3 clause
+    public double getSquare(double cathetusFirst, double cathetusSecond){
+        double square = cathetusFirst * cathetusSecond / 2;
+        return square;
+    }                          // 3 clause
+
+    public int getSummMultiples(){
+        int summMultiples = 0;
+        for(int value : A){
+            if(value%K == 0){
+                summMultiples += value;
+            }
+        }
+        return summMultiples;
+    }                                                                 // 4 clause
+
+    public void showFiveMethodOfString(String str){
+        System.out.println(str.toUpperCase());
+
+        if(str.endsWith("Ld ")){
+            System.out.println(str);
+        }else{
+            System.out.println("Wrong");
+        }
+        for(int i = 0; i < str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+        String [] strings = str.split(" ");
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }                                                // 5 clause
+
+    public void monotoneSequence(double[] arr){
+        int numberIncrease = 0;
+        int numberDecrease = 0;
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i+1]){
+                numberDecrease++;
+                System.out.println("Последовательность не является возрастающей");
+                break;
+            }else if(arr[i] < arr[i+1]){
+                numberIncrease++;
+            }else continue;
+            if(numberDecrease == numberIncrease && numberDecrease != 0){
+                System.out.println("Последовательность не является возрастающей");
+                break;
+            }
+            if(i == arr.length - 2){
+                System.out.println("Последовательность является возрастающей");
+            }
+        }
+    }                                                    // 6 clause
+}
